122192bb3f1696a0097c36af9b67feb4af7bacd3
nawaala <nawaalahmed40@gmail.com>
2023-12-12 15:58:40 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
+import Home from  './Views/Home'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-
-
+      <Route path="/" element={<Home/>} />
       </Routes>
+
     </div>
   );
 }
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,1 +1,14 @@
 import './Home.css'
+
+const Home = () =>{
+    return(
+        <div>
+            <h1>The weather channel</h1>
+            
+      
+
+        </div>
+    )
+}
+
+export default Home
