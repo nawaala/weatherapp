9d72018767b81d3c103ad7e286dc5031b4f0f459
nawaala <nawaalahmed40@gmail.com>
2024-01-02 17:09:43 +0000

    Login

---
+++ src/Views/Login.js
@@ -0,0 +1,48 @@
+import "./AuthPage.css"
+import AuthForm from "../AuthForms/AuthForm"
+import { useNavigate } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react";
+import { useAuth } from "../Contexts/AuthContext";
+
+const Login = ()  => {
+    const naviagte = useNavigate();
+    const [error, setError] = useState()
+
+    const {setUserId, setUsername} = useAuth()
+    const onSubmit = async (e, email, password) => {
+        e.preventDefault();
+
+        const dataToSend = {
+            "email" : email,
+            "password" : password
+        }
+
+        axios.post ("http://localhost:8000/users/login", dataToSend)
+        .then( (res) => {
+            setUsername(res.data.username)
+            setUserId(res.data.id)
+            localStorage.setItem("userId", res.data.id)
+            localStorage.setItem("username", res.data.username)
+
+            navigate("/profile")
+        })
+
+        .catch((err) => {
+            setError(err.response?.data?.detail || err.message || "An unknown error occured")
+        })
+
+    }
+
+    return(
+        <div className="auth-page-container">
+            <h2>Login</h2>
+            {error && <div className="login-error text-label">{error}</div>}
+            <AuthForm onSubmit={onSubmit} formLabel="Sign In" />
+        </div>
+    )
+
+}
+
+export default Login;
+
