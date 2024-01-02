2f8b1c546b3d69824d207b9057ed746fe42934b5
nawaala <nawaalahmed40@gmail.com>
2024-01-02 16:50:58 +0000

    Register

--- src/Views/Register.js
+++ src/Views/Register.js
@@ -2,4 +2,47 @@ import "./AuthPage.css"
 import AuthForm from "../AuthForms/AuthForm"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react"
-import { useAuth } from 
+import { useAuth } from "../Contexts/AuthContext"
+
+import axios from "axios"
+
+const Register = () => {
+    const {setUsername, setUserId} = useAuth ()
+
+    const naviagte = useNavigate()
+    const [error, setError] =useState()
+
+    const onSubmit =async (e, email, password) => {
+        e.preventDefault();
+
+        const dataToSend ={
+            "email" : email,
+            "password" : password
+        }
+
+        axios.post("http://localhost:8000/users/register", dataToSend)
+        .then ((res) => {
+            setUsername(res.data.username)
+            setUserId(res.data.id)
+            localStorage.setItem("userId", res.data.id)
+            localStorage.setItem("username", res.data.username)
+
+            navigate("/profile")
+        })
+        .catch ((err) => {
+            setError(err.response?.data?.detail || err.message || "An unknown error occured")
+        })
+
+
+    }
+
+    return(
+        <div className="auth-page-container">
+            <h2>Register</h2>
+            {error && <div className="login-error text-label">{error}</div>}
+            <AuthForm onSubmit={onSubmit} formLabel="Sign Up"/>
+        </div>
+    )
+}
+
+export default Register;
