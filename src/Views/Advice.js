bf26dd82bb3a775151187778fc8a42ccacb75603
nawaala <nawaalahmed40@gmail.com>
2024-01-10 16:45:06 +0000

    advice

--- src/Views/Advice.js
+++ src/Views/Advice.js
@@ -2,33 +2,31 @@ import "./Advice.css";
 import MenuList from "../Components/MenuList";
 import MenuItem from "../Components/HealthAdviceItem";
 import menu from '../Assets/menu.png';
-
+import Toggle from "../Components/Toggle";
 
 const Advice = () => {
-  return (
-    <div>
-      <div className="advice-title">Advice For you</div>
-      <div className="menu">
-        <div className="menuList">
-          {MenuList.map((menuItem, key) => {
-            return (
-              <div className="p-details" key={key}>
-                <img
-                  className="image-sizes"
-                  src={menuItem.image}
-                  alt={menuItem.name}
-                />
-                <p>{menuItem.details}</p>
-                <h2> Read More
-                  <img src={menu}/>
-                </h2>
-              </div>
-            );
-          })}
+    return (
+      <div>
+        <div className="advice-title"><Toggle/>Advice For you</div>
+        <div className="menu">
+          <div className="menuList">
+            {MenuList.map((menuItem, key) => {
+              return (
+                <div className="p-details" key={key}>
+                  <img
+                    className="image-sizes"
+                    src={menuItem.image}
+                    alt={menuItem.name}
+                  />
+                  <p>{menuItem.details}</p>
+                  <h2> Read more...</h2>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Advice;
+    );
+  };
+  
+  export default Advice;
