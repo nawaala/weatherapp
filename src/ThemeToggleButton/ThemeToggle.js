b5182e66334db57e4094edae3f427914b5371567
nawaala <nawaalahmed40@gmail.com>
2024-01-16 15:45:17 +0000

    theme button complete

--- src/ThemeToggleButton/ThemeToggle.js
+++ src/ThemeToggleButton/ThemeToggle.js
@@ -1,5 +1,16 @@
-import './ThemeToggle.css'
-import { useTheme } from  "../Contexts/ThemeContext.js"
+import "./ThemeToggle.css";
+import { useTheme } from "../Contexts/ThemeContext.js";
+import { ReactComponent as sunIcon } from "../Assets/clear.png";
+import { ReactComponent as darktheme } from "../Assets/nightmode.png";
 
-import lighttheme from '../Assets/clear.png'
-import darktheme from '../Assets/nightmode.png'
+const ThemeToggleButton = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button className={"theme-toggle-button ${theme}"} onClick={toggleTheme}>
+      {theme === "light" ? <darktheme /> : <sunIcon />}
+    </button>
+  );
+};
+
+export default ThemeToggleButton;
