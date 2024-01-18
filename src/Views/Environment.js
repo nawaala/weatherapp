2be6fcb5f3fbc6213ad1698a466bd5b985537403
nawaala <nawaalahmed40@gmail.com>
2024-01-18 11:58:19 +0000

    options

--- src/Views/Environment.js
+++ src/Views/Environment.js
@@ -1,15 +1,42 @@
 import './Environment.css'
 import title from '../Assets/RiskAssessments.jpg'
 
+    var windowText = <div>
+        Open <a href="/">windows</a>, Call for a checkup
+    </div>
+
+var option = [
+    {option: "Too cold", advice: 'Turn on heating, buy Thermal Insulated Door Curtain' },
+    {option: "Too hot", advice: 'Turn on heating, buy Thermal Insulated Door Curtain' },
+    {option: "Mouldy", advice: windowText},
+    {option: "Dusty", advice: 'Turn on heating, buy Thermal Insulated Door Curtain' }
+
+
+    ]    
+    
+
 const Environment = () => {
-    return(
+    return (
+        
         <div>
             <div classname='environment-title'>Risk Assessments
-                <img src={title}/>
-
+                <img src={title} />
+                <select id='SelectCar' onSubmit='displayDetails()'>
+                    <option value="Too cold">Too Cold</option>
+                    <option value="Too hot">Too Hot</option>
+                    <option value="Mould"> Mould</option>
+                    <option value="Dusty">Dusty</option>                   
+                </select>
+                <p id="CarDetail"></p>
             </div>
+
+
+
+            {option[2].advice}
         </div>
     )
+
 };
 
 export default Environment
+
