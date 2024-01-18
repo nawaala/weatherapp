b47f80ec2583bc4452fa40fc9483694c301d9ff5
nawaala <nawaalahmed40@gmail.com>
2024-01-18 12:55:25 +0000

    mouldy

--- src/Views/Environment.js
+++ src/Views/Environment.js
@@ -1,42 +1,63 @@
-import './Environment.css'
-import title from '../Assets/RiskAssessments.jpg'
+import "./Environment.css";
+import title from "../Assets/RiskAssessments.jpg";
 
-    var windowText = <div>
-        Open <a href="/">windows</a>, Call for a checkup
+var Mouldy = (
+  <div>
+    Open <a href="/">windows</a>, Call for a checkup
+  </div>
+);
+
+var windowText = (
+    <div>
+      Open <a href="/">windows</a>, Call for a checkup
     </div>
+  );
+
+    var windowText = (
+    <div>
+    Open <a href="/">windows</a>, Call for a checkup
+    </div>
+    );
 
 var option = [
-    {option: "Too cold", advice: 'Turn on heating, buy Thermal Insulated Door Curtain' },
-    {option: "Too hot", advice: 'Turn on heating, buy Thermal Insulated Door Curtain' },
-    {option: "Mouldy", advice: windowText},
-    {option: "Dusty", advice: 'Turn on heating, buy Thermal Insulated Door Curtain' }
+  {
+    option: "Too cold",
+    advice: "Turn on heating, buy Thermal Insulated Door Curtain",
+  },
+  {
+    option: "Too hot",
+    advice: "Turn on heating, buy Thermal Insulated Door Curtain",
+  },
+  { option: "Mouldy", advice: Mouldy },
+  {
+    option: "Dusty",
+    advice: "Turn on heating, buy Thermal Insulated Door Curtain",
+  },
+
+];
 
 
-    ]    
-    
+
+
 
 const Environment = () => {
-    return (
-        
-        <div>
-            <div classname='environment-title'>Risk Assessments
-                <img src={title} />
-                <select id='SelectCar' onSubmit='displayDetails()'>
-                    <option value="Too cold">Too Cold</option>
-                    <option value="Too hot">Too Hot</option>
-                    <option value="Mould"> Mould</option>
-                    <option value="Dusty">Dusty</option>                   
-                </select>
-                <p id="CarDetail"></p>
-            </div>
+  return (
+    <div>
+      <div classname="environment-title">
+        Risk Assessments
+        <img src={title} />
+        <select id="SelectCar" onSubmit="displayDetails()">
+          <option value="Too cold">Too Cold</option>
+          <option value="Too hot">Too Hot</option>
+          <option value="Mould"> Mould</option>
+          <option value="Dusty">Dusty</option>
+        </select>
+        <p id="CarDetail"></p>
+      </div>
 
-
-
-            {option[2].advice}
-        </div>
-    )
-
+      {option[2].advice}
+    </div>
+  );
 };
 
-export default Environment
-
+export default Environment;
