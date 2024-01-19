f2a4df3f175e31c4cc05cecbb04ffe934ed3bca7
nawaala <nawaalahmed40@gmail.com>
2024-01-19 12:56:16 +0000

    save

--- user_api/models.py
+++ user_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import  AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class AppUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('An email is required')
+        if not password:
+            raise ValueError('A password is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise ValueError()
+
