d05ca1941e2c94e7e5fb68268322d968069f4328
nawaala <nawaalahmed40@gmail.com>
2024-01-25 13:57:11 +0000

    save

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import  AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class AppUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        if not email:
-            raise ValueError('An email is required')
-        if not password:
-            raise ValueError('A password is required')
-        email = self.normalize_email(email)
-        user = self.model(email=email)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password=None):
-        if not email:
-			raise ValueError('An email is required.')	
+	def create_user(self, email, password=None):
+		if not email:
+			raise ValueError('An email is required.')
+		if not password:
+			raise ValueError('A password is required.')
+		email = self.normalize_email(email)
+		user = self.model(email=email)
+		user.set_password(password)
+		user.save()
+		return user
+	def create_superuser(self, email, password=None):
+		if not email:
+			raise ValueError('An email is required.')
 		if not password:
 			raise ValueError('A password is required.')
 		user = self.create_user(email, password)
@@ -34,5 +33,3 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
 	objects = AppUserManager()
 	def __str__(self):
 		return self.username
-            
-
