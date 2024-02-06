4ff45814cb895865b14b31cfc2b6e8314fcea4c1
nawaala <nawaalahmed40@gmail.com>
2024-02-06 10:53:46 +0000

    save

--- src/Components/NavButton.js
+++ src/Components/NavButton.js
@@ -1,9 +1,10 @@
 import { useNavigate } from "react-router-dom";
+
+
 const NavButton = ({text, path, className}) => {
     const navigate = useNavigate()
     return(
         <button className={className} onClick={() => navigate(path)}><span className="text-headline">{text}</span></button>
     )
 }
-
 export default NavButton;
